c4625d7c0599f0a1015422edf254b2a7cd326a37
ReckURmama <kmohd002@gmail.com>
2025-03-12 17:42:37 -0400

    commit new

--- src/test/java/edu/canisius/cyb/cyb600/lab2/Task3UnitTests.java
+++ src/test/java/edu/canisius/cyb/cyb600/lab2/Task3UnitTests.java
@@ -2,51 +2,61 @@ package edu.canisius.cyb.cyb600.lab2;
 
 import org.junit.Before;
 import org.junit.Test;
-
+import java.util.List;
+import java.util.Arrays;
+import static org.junit.Assert.*;
 
 public class Task3UnitTests {
-    private Task3 task3;
+    private Task3 instance;
 
     @Before
     /*
-    STUDENTS SHOULD NOT BE CHANGING THIS METHOD.
+    DO NOT MODIFY THIS METHOD.
      */
-    public void setUp() {
-        this.task3 = new Task3();
+    public void initialize() {
+        this.instance = new Task3();
     }
 
     @Test
-    public void test_removeDuplicates_best(){
-        // Student should write tests here
+    public void testUniqueCharacterExtraction_bestCase(){
+        String input = "software";
+        List<String> expectedOutput = Arrays.asList("a", "r", "s", "t", "e", "f", "w", "o");
+        List<String> actualOutput = instance.removeDuplicates(input);
+        assertEquals("Unexpected result:", expectedOutput, actualOutput);
     }
 
     @Test
-    public void test_removeDuplicates_average(){
-        // Student should write tests here
+    public void testUniqueCharacterExtraction_averageCase(){
+        String input = "cybersecurity";
+        List<String> expectedOutput = Arrays.asList("b","r","c","s","t","e","u","y","i");
+        List<String> actualOutput = instance.removeDuplicates(input);
+        assertEquals("Unexpected result:", expectedOutput, actualOutput);
     }
 
     @Test
-    public void test_removeDuplicates_worst(){
-        // Student should write tests here
-
+    public void testUniqueCharacterExtraction_worstCase(){
+        String input = "nnnnnnnnnnnnnnnnnnnnnn";
+        List<String> expectedOutput = Arrays.asList("n");
+        List<String> actualOutput = instance.removeDuplicates(input);
+        assertEquals("Unexpected result:", expectedOutput, actualOutput);
     }
 
     @Test
-    public void test_areTheseAnagrams_best(){
-        // Student should write tests here
-
+    public void testAnagramCheck_bestCase(){
+        assertTrue(instance.areTheseAnagrams("tac","cat"));
+        assertFalse(instance.areTheseAnagrams("tic","tac"));
     }
 
     @Test
-    public void test_areTheseAnagrams_average(){
-        // Student should write tests here
-
+    public void testAnagramCheck_averageCase(){
+        assertTrue(instance.areTheseAnagrams("java", "jvaa"));
+        assertTrue(instance.areTheseAnagrams("javascript", "scriptjava"));
+        assertFalse(instance.areTheseAnagrams("nonsense", "noworld"));
     }
 
     @Test
-    public void test_areTheseAnagrams_worst(){
-        // Student should write tests here
-
+    public void testAnagramCheck_worstCase(){
+        assertTrue(instance.areTheseAnagrams("dormitory", "dirtyroom"));
+        assertFalse(instance.areTheseAnagrams(null, "really long word"));
     }
-
 }
